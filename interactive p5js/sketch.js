282836dacc88b7fc87c514ceedd8f121aff12844
goodman <udaysandhu2005@outlook.com>
2022-09-20 11:46:17 -0600

    added the flaggies
    thing

---
+++ interactive p5js/sketch.js
@@ -0,0 +1,18 @@
+// Interactive p5js project
+// Uday Sandhu
+// September 19th 2022
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let flag;
+function preload() {
+  flag = loadImage("flag-icons-main/flags/4x3/ar.svg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  image(flag, 0, 0);
+}
+
+function draw() {
+}
