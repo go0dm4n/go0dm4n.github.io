2d6b40e71b000b49d4cee480d73880017c6ed50e
goodman <udaysandhu2005@outlook.com>
2022-10-05 12:17:01 -0600

    IT WORKS

--- flags/sketch.js
+++ flags/sketch.js
@@ -13,14 +13,15 @@ let fnames;
 let fname = "Canada";
 let textsize = 50;
 let total = 209;
-let gameon = false; // game state
+let state = "main"; // game state
 let score = 0;
 let progress = 0;
+
 let correct;
-let c1;
-let c2;
-let c3;
-let c4;
+let c1 = "";
+let c2 = "";
+let c3 = "";
+let c4 = "";
 
 function preload() {
   bgi = loadImage("allflags.png");
@@ -31,40 +32,20 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
-  doflag();
 
   image(bgi, 0, 0, windowWidth, windowHeight); // background image
 
-  image(flag, windowWidth/2 - flag.width/2, windowHeight/2 - flag.height/2); // main flag
 }
 
 function draw() {
   mainmenu();
-  drawScore();
-}
-
-function doflag() {
-  preload();
-}
-
-function drawScore() {
-  if (gameon === true){
-    fill(255);
-    rect(20, 10, textsize * 3, 50); // background rectangle for score
-
-    fill(0);
-    textSize(textsize);
-    text(score, 20, 0 + textsize); // score text
-
-    textSize(textsize);
-    text("/" + total, 60 , 0 + textsize); // denominator
-  }
+  drawStuff();
+  randflag();
 }
 
 function mainmenu(){
-  if (gameon === false){
-    image(logo, windowWidth/2 - logo.width/2, windowHeight/2 - logo.height/2 - 150); // logo
+  if (state === "main"){
+    image(logo, windowWidth/2 - logo.width/2.6, windowHeight/2 - logo.height/3 - 150, logo.width/1.3, logo.height/1.3); // logo
 
     fill(255);
     rect(windowWidth/2 - logo.width/2, logo.height, logo.width, 50); // start button rectangle
@@ -74,13 +55,62 @@ function mainmenu(){
     text("Start Game", windowWidth/2 - logo.width/2, logo.height + textsize); // start text
   }
 }
+function drawStuff() {
+  if (state === "game"){
+
+    image(bgi, 0, 0, windowWidth, windowHeight); // background image
+    fill(255);
+    rect(20, 10, textsize * 3, 50); // background rectangle for score
+
+    fill(0);
+    textSize(textsize);
+    text(score, 20, 0 + textsize); // score text
+
+    textSize(textsize);
+    text("/" + total, 60 , 0 + textsize); // denominator
+
+    fill(255);
+    rect(windowWidth/2 - logo.width/4, logo.height - 70, logo.width /2, 50); // option 4 rectangle
+   
+    fill(0);
+    textSize(textsize);
+    text(c4, windowWidth/2 - logo.width/4, logo.height - 70 + textsize); // score text
+
+    fill(255);
+    rect(windowWidth/2 - logo.width/4, logo.height - 140 , logo.width /2, 50); // option 3 rectangle
+    
+    fill(0);
+    textSize(textsize);
+    text(c2, windowWidth/2 - logo.width/4, logo.height - 140 + textsize); // score text
+ 
+    fill(255);
+    rect(windowWidth/2 - logo.width/4, logo.height - 210, logo.width /2, 50); // option 2 rectangle
+   
+    fill(0);
+    textSize(textsize);
+    text(c3, windowWidth/2 - logo.width/4, logo.height - 210 + textsize); // score text
+
+    fill(255);
+    rect(windowWidth/2 - logo.width/4, logo.height - 280, logo.width /2, 50); // option 1 rectangle
+    
+    fill(0);
+    textSize(textsize);
+    text(c1, windowWidth/2 - logo.width/4, logo.height - 280 + textsize); // score text
+  }  
+}
+
+function mouseIn(left, right, top, bottom){
+  return mouseX >= left && mouseX <= right && 
+  mouseY >= top && mouseY <= bottom;
+}
 
 function mousePressed(){
-  if (gameon === false && mouseIn(windowWidth/2 - logo.width/2, windowWidth/2 - logo.width/2 + logo.width, logo.height, logo.width + 50)){ // start button
-    gameon = true;
+  if (state === "main" && mouseIn(windowWidth/2 - logo.width/2, windowWidth/2 - logo.width/2 + logo.width, logo.height, logo.width + 50)){ // start button
+    state = "switch";
   }
 
-  if (gameon === true && mouseIn("INSERT PARAMETERS FOR C1")) { // option 1
+  if (state === "game" && mouseIn("INSERT PARAMETERS FOR C1")) { // option 1
+    fill(255);
     if (correct === c1) {
       console.log("banana");
       score += 1;
@@ -91,9 +121,9 @@ function mousePressed(){
     }
   }
 
-  if (gameon === true && mouseIn("INSERT PARAMETERS FOR C2")) { // option 2
+  if (state === "game" && mouseIn("INSERT PARAMETERS FOR C2")) { // option 2
     if (correct === c2) {
-      console.log("banana");
+      console.log(fname);
       score += 1;
       progress += 1;
     }
@@ -102,7 +132,7 @@ function mousePressed(){
     }
   }
 
-  if (gameon === true && mouseIn("INSERT PARAMETERS FOR C3")) {
+  if (state === "game" && mouseIn("INSERT PARAMETERS FOR C3")) {
     if (correct === c3) {
       console.log("banana");
       score += 1;
@@ -112,32 +142,31 @@ function mousePressed(){
       progress += 1;
     }
   }
-  if (gameon === true && mouseIn("INSERT PARAMETERS FOR C4")){
+  if (state === "game" && mouseIn("INSERT PARAMETERS FOR C4")){
     if (correct === c4) {
       console.log("banana");
     }
   }
 }
 
+function doflag() {
+  preload();
+  image(flag, windowWidth/2 - flag.width/2, windowHeight/2 - flag.height/2); // main flag
+}
+
 function randflag(){
-  if (gameon === true) {
+  if (state === "switch") {
     let options = [c1, c2, c3, c4];
-    correct = options[1]; // picks a random button to be correct
-    c1 = fnames[1]; // rand flag for option 1
-    c2 = fnames[1]; // rand flag for option 2
-    c3 = fnames[1]; // rand flag for option 3
-    c4 = fnames[1]; // rand flag for option 4
+    correct = options[random(0, 3)]; // picks a random button to be correct
+    c1 = fnames[Math.floor(random(0, 209))]; // rand flag for option 1
+    c2 = fnames[Math.floor(random(0, 209))]; // rand flag for option 2
+    c3 = fnames[Math.floor(random(0, 209))]; // rand flag for option 3
+    c4 = fnames[Math.floor(random(0, 209))]; // rand flag for option 4
     fname = correct; // flag thats drawn is based off of what button was chosen to be correct
+    console.log(correct);
+    state = "game";
+    doflag();
   }
 }
 
-function mouseIn(left, right, top, bottom){
-  return mouseX >= left && mouseX <= right && 
-  mouseY >= top && mouseY <= bottom;
-}
 
-function randInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  Math.floor(Math.random() * (max - min) + min);
-}
