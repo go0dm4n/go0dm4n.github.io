fb6330f251ef27119acc77a58f955c98384c0cbc
goodman <udaysandhu2005@outlook.com>
2022-11-04 11:16:57 -0600

    thingsa

--- 2darray project/sketch.js
+++ 2darray project/sketch.js
@@ -21,6 +21,8 @@ let mouse = "pencil";
 let xPos;
 let yPos;
 
+let state = "main"
+
 function preload(){
   levels = loadStrings("levels/levellist.txt");
 }
@@ -39,6 +41,37 @@ function draw() {
   checkCross();
   
 }
+
+function mainMenu() {
+  if (state === "main"){
+    rect 
+  }
+}
+
+function mouseIn(left, right, top, bottom){ //button parameter function
+  return mouseX >= left && mouseX <= right && 
+  mouseY >= top && mouseY <= bottom;
+}
+
+function makeButton(x, y, width, height, rectcolor, textcolor, textsize, textc){ //draws rectangle with text
+
+  if (mouseIn(x, x + width, y, y + height)){ //darkens rectangle if mouse is in
+    rectcolor -= 50;
+  }
+
+  if (!mouseIn(x, x + width, y, y + height)){ //lightens rectangle if mouse is out
+    rectcolor += 50;
+  }
+
+  fill(rectcolor);
+  rect(x, y, width, height); //draws rect
+
+  fill(textcolor);
+  textSize(textsize);
+  text(textc, x, y + textsize - 5); //writes text
+
+}
+
 
 function makeCross(){
   for (let y = 0; y < cols; y++) {
@@ -59,14 +92,14 @@ function drawCross(){
       else if (grid[y][x] === 1 ) {
         fill("black");
       }
-      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+      rect(x*cellSize + cols*(cellSize*1.5), y*cellSize + height/4, cellSize, cellSize);
     }
   }
 }
 
 function checkCross(){
-  xPos = Math.floor(mouseX/cellSize);
-  yPos = Math.floor(mouseY/cellSize);
+  xPos = Math.floor(mouseX/cellSize + cols*(cellSize*1.5));
+  yPos = Math.floor(mouseY/cellSize + height/4);
   // if (grid[yPos][xPos] === 0 && newGrid[yPos][xPos] === 0) {
   //   console.log("baba");
   // }
@@ -88,15 +121,27 @@ function clicky(){
     
   }
 }
+
 function doLevels(name){
-  index = levels.indexOf(name);
+  index = levels.indexOf("note");
   leveln = levels[index];
   level = loadJSON("levels/" + leveln + ".json");
   grid = level;
-  // for (let i = 0; )
+  for (let i = 0; i < grid.length; i++) {
+    newGrid.push(grid[i]);
+    for (let k = 0; k < grid[i].length; k++) {
+      newGrid.push(grid[i][k]);
+    }
+  }
 }
 
 function keyPressed(){
+  if (key === "n"){
+    doLevels("note");
+  }
+  if (key === "q"){
+    doLevels("questionmark");
+  }
   if (key === "n"){
     doLevels("note");
   }
